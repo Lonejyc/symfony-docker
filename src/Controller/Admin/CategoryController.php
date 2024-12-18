6001acdd393c0f8300255d957243e4d828ccfd49
Lonejyc <jocelyn.marcilloux@gmail.com>
2024-12-18 10:37:27 +0100

    feat: admin path

--- src/Controller/Admin/CategoryController.php
+++ src/Controller/Admin/CategoryController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use App\Entity\Category;
 use App\Form\CategoryType;
@@ -10,33 +10,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
+#[Route('/admin/categories', name: 'admin.category.')]
 class CategoryController extends AbstractController {
-    #[Route('/categories', name: 'category.index')]
-    public function index(Request $request, CategoryRepository $repository): Response
+    #[Route('/', name: 'index')]
+    public function index( CategoryRepository $repository): Response
     {
         $categories = $repository->findAll();
-        return $this->render('category/index.html.twig', [
+        return $this->render('admin/category/index.html.twig', [
             'categories' => $categories
         ]);
     }
 
-    #[Route('/categories/{slug}-{id}', name: 'category.show', requirements: ['slug' => '[a-z0-9\-]+', 'id' => '\d+'])]
-    public function show(Request $request, string $slug, int $id, CategoryRepository $repository): Response
-    {
-        $category = $repository->find($id);
-        if ($category->getSlug() !== $slug) {
-            return $this->redirectToRoute('category.show', [
-                'id' => $category->getId(),
-                'slug' => $category->getSlug()
-            ], 301);
-        }
-        return $this->render('category/show.html.twig', [
-            'category' => $category
-        ]);
-    }
-
-    #[Route('/categories/{id}/edit', name: 'category.edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}', name: 'edit', requirements: ['id' => Requirement::DIGITS], methods: ['GET', 'POST'])]
     public function edit(Category $category, Request $request, EntityManagerInterface $em)
     {
         $form = $this->createForm(CategoryType::class, $category);
@@ -44,15 +31,15 @@ class CategoryController extends AbstractController {
         if ($form->isSubmitted() && $form->isValid()) {
             $em->flush();
             $this->addFlash('success', 'Catégorie modifiée avec succès');
-            return $this->redirectToRoute('category.index');
+            return $this->redirectToRoute('admin.category.index');
         }
-        return $this->render('category/edit.html.twig', [
+        return $this->render('admin/category/edit.html.twig', [
             'category' => $category,
             'formContent' => $form
         ]);
     }
 
-    #[Route('/categories/create', name: 'category.create')]
+    #[Route('/create', name: 'create')]
     public function create(Request $request, EntityManagerInterface $em)
     {
         $category = new Category();
@@ -62,19 +49,19 @@ class CategoryController extends AbstractController {
             $em->persist($category);
             $em->flush();
             $this->addFlash('success', 'Catégorie créée avec succès');
-            return $this->redirectToRoute('category.index');
+            return $this->redirectToRoute('admin.category.index');
         }
-        return $this->render('category/create.html.twig', [
+        return $this->render('admin/category/create.html.twig', [
             'formContent' => $form
         ]);
     }
 
-    #[Route('/categories/{id}/delete', name: 'category.delete', methods: ['DELETE'])]
+    #[Route('/{id}', name: 'delete', requirements: ['id' => Requirement::DIGITS], methods: ['DELETE'])]
     public function delete(Category $category, EntityManagerInterface $em)
     {
         $em->remove($category);
         $em->flush();
         $this->addFlash('success', 'Catégorie supprimée avec succès');
-        return $this->redirectToRoute('category.index');
+        return $this->redirectToRoute('admin.category.index');
     }
 }
--- src/Controller/Admin/RecipeController.php
+++ src/Controller/Admin/RecipeController.php
@@ -1,42 +1,28 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use App\Entity\Recipe;
 use App\Form\RecipeType;
-use App\Repository\CategoryRepository;
 use App\Repository\RecipeRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Requirement\Requirement;
 
+#[Route('/admin/recettes', name: 'admin.recipe.')]
 class RecipeController extends AbstractController {
-    #[Route('/recettes', name: 'recipe.index')]
-    public function index(Request $request, RecipeRepository $repository): Response
+    #[Route('/', name: 'index')]
+    public function index(RecipeRepository $repository): Response
     {
         $recipes = $repository->findWithDurationLowerThan(30);
-        return $this->render('recipe/index.html.twig', [
+        return $this->render('admin/recipe/index.html.twig', [
             'recipes' => $recipes
         ]);
     }
-    #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['slug' => '[a-z0-9\-]+', 'id' => '\d+'])]
-    public function show(Request $request, string $slug, int $id, RecipeRepository $repository): Response
-    {
-        $recipe = $repository->find($id);
-        if ($recipe->getSlug() !== $slug) {
-            return $this->redirectToRoute('recipe.show', [
-                'id' => $recipe->getId(),
-                'slug' => $recipe->getSlug()
-            ], 301);
-        }
-        return $this->render('recipe/show.html.twig', [
-            'recipe' => $recipe
-        ]);
-    }
-
-    #[Route('/recettes/{id}/edit', name: 'recipe.edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}', name: 'edit', requirements: ['id' => Requirement::DIGITS], methods: ['GET', 'POST'])]
     public function edit(Recipe $recipe, Request $request, EntityManagerInterface $em)
     {
         $form = $this->createForm(RecipeType::class, $recipe);
@@ -44,15 +30,15 @@ class RecipeController extends AbstractController {
         if ($form->isSubmitted() && $form->isValid()) {
             $em->flush();
             $this->addFlash('success', 'Recette modifiée avec succès');
-            return $this->redirectToRoute('recipe.index');
+            return $this->redirectToRoute('admin.recipe.index');
         }
-        return $this->render('recipe/edit.html.twig', [
+        return $this->render('admin/recipe/edit.html.twig', [
             'recipe' => $recipe,
             'formContent' => $form
         ]);
     }
 
-    #[Route('/recettes/create', name: 'recipe.create')]
+    #[Route('/create', name: 'create')]
     public function create(Request $request, EntityManagerInterface $em)
     {
         $recipe = new Recipe();
@@ -62,19 +48,19 @@ class RecipeController extends AbstractController {
             $em->persist($recipe);
             $em->flush();
             $this->addFlash('success', 'Recette créée avec succès');
-            return $this->redirectToRoute('recipe.index');
+            return $this->redirectToRoute('admin.recipe.index');
         }
-        return $this->render('recipe/create.html.twig', [
+        return $this->render('admin/recipe/create.html.twig', [
             'formContent' => $form
         ]);
     }
 
-    #[Route('/recettes/{id}/edit', name: 'recipe.delete', methods: ['DELETE'])]
+    #[Route('/{id}', name: 'delete', requirements: ['id' => Requirement::DIGITS], methods: ['DELETE'])]
     public function delete(Recipe $recipe, EntityManagerInterface $em)
     {
         $em->remove($recipe);
         $em->flush();
         $this->addFlash('success', 'Recette supprimée avec succès');
-        return $this->redirectToRoute('recipe.index');
+        return $this->redirectToRoute('admin.recipe.index');
     }
 }
